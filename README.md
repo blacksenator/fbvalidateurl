26cb877b24a5126aeaa80cb056c715e95effe6b3
Black Senator <knuffy@anasco.de>
2019-04-24 21:34:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # fbvalidateurl
+
+If you want to make a program available that access the interfaces of the FRITZ!Box and you want to ensure that the user has the freedom to enter his router address with or without schema, with host name or IP, then this routine could be useful ,
+Not valid addresses lead to an exception. If successful, you get an array corresponding to [parse_url](https://www.php.net/manual/en/function.parse-url.php).
+
+## Requirements
+
+  * PHP 7.0
+  * Composer (follow the installation guide at https://getcomposer.org/download/)
+
+## Installation
+
+You can install it through Composer:
+
+    "require": {
+        "blacksenator/fbvalidateurl": "dev-master#1.0"
+    },
+
+or
+
+    git clone https://github.com/blacksenator/fbvalidateurl.git
+
+## License
+This script is released under MIT license.
+
+## Authors
+Copyright (c) 2019 Volker Püschel
---
+++ src/fbvalidateurl.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace blacksenator\fbvalidateurl;
+
+/**
+  * The class provides a function to validate and get the URL
+  * structur for FRITZ!Box router
+  * Example:
+  * $url => [
+  *     ['scheme'] => '',   // 'http' or 'https'
+  *     ['host']   => **',  // your given IP or 'fritz.box'
+  *     ['port']   => ''
+  * ]
+  *
+  * @author Volker Püschel <knuffy@anasco.de>
+  * @copyright Volker Püschel 2019
+  * @license MIT
+ **/
+
+class fbvalidateurl
+{
+    private $url = [];
+
+    /**
+     * get an array of valid URL
+     *
+     * @param string $url
+     * @return array $this->url
+     */
+    public function getValidURL($url)
+    {
+        $errorMessage = sprintf('Validation error! %s is not a valid URL!', $url);
+        $this->url = parse_url($url);
+        if (!isset($this->url['host'])) {
+            if (!isset($this->url['path'])) {
+                throw new \Exception($errorMessage);
+            } else {
+                if (!inet_pton($this->url['path'])) {
+                    throw new \Exception($errorMessage);
+                } else {
+                    $this->url['host'] = $this->url['path'];
+                }
+            }
+        } else {
+            if ($this->url['host'] != 'fritz.box' && !inet_pton($this->url['host'])) {
+                throw new \Exception($errorMessage);
+            }
+        }
+        if (!isset($this->url['scheme'])) {
+            if (!isset($this->url['port'])) {
+                $this->url['scheme'] = 'http';
+            } else {
+                if ($this->url['port'] == '443') {
+                    $this->url['scheme'] = 'https';
+                } else {
+                    $this->url['scheme'] = 'http';
+                }
+            }
+        } else {
+            if (isset($this->url['port'])) {
+                if ($this->url['scheme'] == 'http' && $this->url['port'] == '443') {
+                    throw new \Exception($errorMessage);
+                }
+                if ($this->url['scheme'] == 'https' && $this->url['port'] == '80') {
+                    throw new \Exception($errorMessage);
+                }
+
+            }
+        }
+
+        return $this->url;
+    }
+}
